a920f6cc3ee0bb661ddd1569d5801eb5f76767a6
RamBoFe <rambof@gmail.com>
2019-06-03 13:50:58 +0200

    Added torrent-search-api package and use for YggTorrent.

--- routes/torrents/ygg.mjs
+++ routes/torrents/ygg.mjs
@@ -1,9 +1,12 @@
 import Router from 'koa-router';
+import torrentSearch from 'torrent-search-api';
+
+torrentSearch.enableProvider('YggTorrent', 'chancette', 'rivenBot19!');
 
 const router = new Router();
-
-router.get('/', async (ctx) => {
-  ctx.body = 'Torrents results YggTorrents !!!';
+router.get('/search', async (ctx) => {
+  const { search } = ctx.query;
+  ctx.body = await torrentSearch.search(search);
 });
 
 export default router.routes();
