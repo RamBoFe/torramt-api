488dd2a6805beb623e7bfe920f8cd39522001b9b
RamBoFe <rambof@gmail.com>
2020-02-16 12:04:45 +0100

    Https : back end uses https if a path certificate variable is given else http is used.

--- server.mjs
+++ server.mjs
@@ -1,9 +1,24 @@
 import Koa from 'koa';
 import cors from '@koa/cors';
+import fs from 'fs';
+import https from 'https';
 import routes from './routes/index';
+import config from './services/config';
 
 const app = new Koa();
+const PORT = 2223;
 
 app.use(cors());
 app.use(routes);
-app.listen(2223);
+
+if (config.get('path_certificate')) {
+  const credentials = {
+    key: fs.readFileSync(`${config.get('path_certificate')}/privkey.pem`, 'utf8'),
+    cert: fs.readFileSync(`${config.get('path_certificate')}/cert.pem`, 'utf8'),
+    ca: fs.readFileSync(`${config.get('path_certificate')}/chain.pem`, 'utf8'),
+  };
+
+  https.createServer(credentials, app.callback()).listen(PORT);
+} else {
+  app.listen(PORT);
+}
