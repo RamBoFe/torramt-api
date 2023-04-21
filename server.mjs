fb8058f19c5dc35a3984bf9cc8eb41fdf6f6c16f
RamBoFe <rambof@gmail.com>
2023-04-21 10:42:24 +0200

    Https removed for the backend because it is now placed behind a proxy.

--- server.mjs
+++ server.mjs
@@ -1,8 +1,6 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import cors from '@koa/cors';
-import fs from 'fs';
-import https from 'https';
 import routes from './routes/index.mjs';
 import config from './services/config.mjs';
 
@@ -12,22 +10,7 @@ app.use(cors());
 app.use(bodyParser());
 app.use(routes);
 
-if (config.get('node:protocol') === 'https') {
-  const credentials = {
-    key: config.get('node:https:key')
-      ? fs.readFileSync(config.get('node:https:key'), 'utf8')
-      : undefined,
-    cert: config.get('node:https:cert')
-      ? fs.readFileSync(config.get('node:https:cert'), 'utf8')
-      : undefined,
-    ca: config.get('node:https:ca')
-      ? fs.readFileSync(config.get('node:https:ca'), 'utf8')
-      : undefined,
-  };
+app.listen(config.get('node:port'), () => {
+  console.log(`Api Torrant listening on http://${config.get('node:domain')}:${config.get('node:port')}`);
+});
 
-  https.createServer(credentials, app.callback()).listen(config.get('node:port'));
-} else {
-  app.listen(config.get('node:port'));
-}
-
-console.log(`Api Torrant listening on ${config.get('node:protocol')}://${config.get('node:domain')}:${config.get('node:port')}`);
