13f5ac87e2160fbda01a47fc3c0afd80afc8a541
RamBoFe <rambof@gmail.com>
2024-01-27 01:33:01 +0100

    Console.log deleted.

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,6 @@
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import cors from "@koa/cors";
-
-// import environment from "../services/environment.service";
-// import yggSrv from "./torrent-providers/yggtorrent.provider";
-// import torrentSrv from "../services/torrent.service";
-
 import routeSrv from "./services/route.service.ts";
 import environment from "./services/environment.service.ts";
 
@@ -15,7 +10,6 @@ export default class Api {
 
     app.use(cors());
     app.use(bodyParser());
-    console.log(routeSrv.getRoutes());
     app.use(routeSrv.getRoutes());
 
     app.listen(environment.get("node:port"), async () => {
