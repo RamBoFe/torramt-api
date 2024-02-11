66a3c34b57e73b13f2670751770b33a708fa8613
RamBoFe <rambof@gmail.com>
2024-02-11 23:48:14 +0100

    Work in progress...

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,9 @@
+import cors from "@koa/cors";
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
-import cors from "@koa/cors";
-import routeSrv from "./services/route.service.ts";
 import environment from "./services/environment.service.ts";
+import firebaseSrv from "./services/firebase.service.ts";
+import routeSrv from "./services/route.service.ts";
 
 export default class Api {
   static init(): void {
@@ -11,7 +12,7 @@ export default class Api {
     app.use(cors());
     app.use(bodyParser());
     app.use(routeSrv.getRoutes());
-
+    const ok = firebaseSrv;
     app.listen(environment.get("node:port"), async () => {
       console.log(
         `Api Torrant listening on http://${environment.get(
--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -10,6 +10,14 @@ export class FirebaseService {
     const ok: app.App = firebase.default.initializeApp({
       credential: firebase.default.credential.cert(serviceAccount),
     });
+
+    firebase.default
+      .auth()
+      .verifyIdToken(
+        "eyJhbGciOiJSUzI1NiIsImtpZCI6ImFlYzU4NjcwNGNhOTZiZDcwMzZiMmYwZDI4MGY5NDlmM2E5NzZkMzgiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiR2xvcmlvbiBHcsOpZ29yeSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NLOV9tZF9jSW9fNVE5RUk5ZzNJcXpzbEhvcFR3ckdlaFZ1YjdHcmx6QlE9czk2LWMiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vdG9ycmFtdC04MzFlZSIsImF1ZCI6InRvcnJhbXQtODMxZWUiLCJhdXRoX3RpbWUiOjE3MDc2OTA3OTQsInVzZXJfaWQiOiJmUnAyZ2RoeXl5Y1BoSVY1RWxJbFVORE1ReUYzIiwic3ViIjoiZlJwMmdkaHl5eWNQaElWNUVsSWxVTkRNUXlGMyIsImlhdCI6MTcwNzY5MDc5NCwiZXhwIjoxNzA3Njk0Mzk0LCJlbWFpbCI6InJhbWJvZkBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJnb29nbGUuY29tIjpbIjExMTQ1OTkzNzI4MzM2OTU4MDIyMSJdLCJlbWFpbCI6WyJyYW1ib2ZAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoiZ29vZ2xlLmNvbSJ9fQ.mmWTDohIhYpWJqY7Xs_qkVk1jaPo30LbJohhJvgxFK-0ueFY6wWj7y7TvOrfjZhAwGtfXnGsQYg4ssO6GNYZAlEHynxbKPFJjb1ohbgvYaQ1JY_qDQvDUieIsz2d-9kyLeKRbeEVbgx3ANzszObLDMOhcSh_IWKyb9iITXUvHDHMivJB4eQ4gvyMa9KrPz2zPVsRNObyyv8FqtBw4GdY255inpjNONRHWt7-x8N-KCUWEZeoICR1SSnJN25vl-csG6mg56o811JbILeyPCB92FAcCaNF3j06M1fMtj4Rpy25dy6xmNFq-nVC1kJT5OXapMNL7Wr_m-cZrLc39qNgRg",
+      )
+      .then((r) => console.log(r))
+      .catch((e) => console.log(e));
   }
 }
 
